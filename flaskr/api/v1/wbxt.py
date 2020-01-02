c3bed0a58e150e82e767cd58d73c9f0377ba1e75
Markus Schneider <marschne@cisco.com>
2020-01-01 22:14:07 -0500

    Minor docs cleanup

--- flaskr/api/v1/wbxt.py
+++ flaskr/api/v1/wbxt.py
@@ -18,11 +18,14 @@ get_rooms_args.add_argument('max', type=int, required=False,
 
 @api.route("/rooms")
 class wbxt_get_rooms_api(Resource):
-    @api.expect(get_rooms_args)
+    @api.expect(get_rooms_args)    
     def get(self):
+        '''
+        Retrieves Webex Teams Rooms
+        '''
         args = request.args.to_dict()
         wbxt = WBXT()
-        return wbxt.get_rooms()
+        return wbxt.get_rooms(parameters=args)
 
 
 message_post_args = reqparse.RequestParser()
@@ -36,6 +39,9 @@ message_post_args.add_argument('markdown', type=str, required=False,
 class wbxt_create_message_api(Resource):
     @api.expect(message_post_args)
     def post(self):
+        '''
+        Sends a message to a Webex Teams Rooms using the Room ID.
+        '''
         args = request.args.to_dict()
         wbxt = WBXT()
         return wbxt.create_message(payload=args)
