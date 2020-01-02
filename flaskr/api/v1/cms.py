0817bdc8d9a84960e99c7a564b45ec9c2b858c7e
Markus Schneider <marschne@cisco.com>
2020-01-01 22:37:01 -0500

    CMS SW version checker fix

--- flaskr/api/v1/cms.py
+++ flaskr/api/v1/cms.py
@@ -35,7 +35,7 @@ class cms_version_api(Resource):
         cms = CMS(default_cms['host'], default_cms['username'], default_cms['password'], port=default_cms['port'])
         cms_status = cms.get_system_status()
         if cms_status['success']:
-            return cms_status['message']['status']['softwareVersion']
+            return cms_status['response']['status']['softwareVersion']
         return cms_status
 
 
