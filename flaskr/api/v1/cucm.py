5ef6ea7fca5719613d1e23a6077b3178dc0b58df
Baha Akman <makman@cisco.com>
2020-01-06 22:02:43 -0500

    Grouped all Phone Endpoints under /phone/<device_name>

--- flaskr/api/v1/cucm.py
+++ flaskr/api/v1/cucm.py
@@ -1,3 +1,4 @@
+import re
 from flask import jsonify
 from flask import request
 from flask import Blueprint
@@ -53,38 +54,49 @@ class cucm_get_version_api(Resource):
         return jsonify(result)
 
 
-@api.route("/add_phone")
-class cucm_add_phone_api(Resource):
+@api.route("/phone/<string:device_name>")
+@api.param('device_name', description='The Name of the Phone Device')
+class cucm_phone_api(Resource):
     @api.expect(cucm_add_phone_query_args, validate=True)
-    def post(self):
+    def post(self, device_name):
         """
-        Adds a new Phone to CUCM
+        Adds a new Phone Device to CUCM
 
-        This API method executes an addPhone AXL Request with the supplied parameters
-
-        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_addPhone.html#Link284
+        This API method utilizes AXL addPhone and getUser requests along with the supplied parameters
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_addPhone.html
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_getUser.html
         """
         try:
             cucm_add_phone_query_parsed_args = cucm_add_phone_query_args.parse_args(request)
+            axl_get_user_result = myAXL.get_user(userid=cucm_add_phone_query_parsed_args['ownerUserName'])
+            user_telephoneNumber = None
+            if axl_get_user_result['return']['user'].get('telephoneNumber'):
+                user_telephoneNumber = re.sub(r"^\+", "\\+", axl_get_user_result['return']['user']['telephoneNumber'])
+            user_associatedDevices = []
+            if axl_get_user_result['return']['user']['associatedDevices']:
+                user_associatedDevices = axl_get_user_result['return']['user']['associatedDevices']['device']
+
             cucm_add_phone_data_dict = {
-                "name": cucm_add_phone_query_parsed_args['name'],
+                "name": device_name,
                 "description": cucm_add_phone_query_parsed_args['description'],
-                "product": "Cisco Unified Client Services Framework",
+                "product": cucm_add_phone_query_parsed_args['phonetype'],
                 "class": "Phone",
                 "protocol": "SIP",
                 "protocolSide": "User",
                 "commonPhoneConfigName": "Standard Common Phone Profile",
                 "devicePoolName": "Default",
                 "locationName": "Hub_None",
-                "securityProfileName": "Cisco Unified Client Services Framework - Standard SIP Non-Secure Profile",
+                "securityProfileName": "Universal Device Template - Model-independent Security Profile",
                 "sipProfileName": "Standard SIP Profile",
                 "ownerUserName": cucm_add_phone_query_parsed_args['ownerUserName'],
                 "lines": {
                     "line": {
                         "index": "1",
                         "dirn": {
-                            "pattern": cucm_add_phone_query_parsed_args['directorynumber'],
-                            "routePartitionName": cucm_add_phone_query_parsed_args['routepartition']
+                            "pattern": user_telephoneNumber,
+                            "routePartitionName": 'DN_PT'
                         },
                         "display": cucm_add_phone_query_parsed_args['calleridname'],
                         "displayAscii": cucm_add_phone_query_parsed_args['calleridname'],
@@ -96,19 +108,31 @@ class cucm_add_phone_api(Resource):
                     }
                 }
             }
-            axlresult = myAXL.add_phone(phone_data=cucm_add_phone_data_dict)
+
+            axl_add_phone_result = myAXL.add_phone(phone_data=cucm_add_phone_data_dict)
+            user_associatedDevices.append(device_name)
+            cucm_update_user_data_dict = {
+                "userid": cucm_add_phone_query_parsed_args['ownerUserName'],
+                "associatedDevices": {
+                    'device': user_associatedDevices
+                }
+            }
+            axl_update_user_result = myAXL.update_user(user_data=cucm_update_user_data_dict)
         except Exception as e:
             apiresult = {'success': False, 'message': str(e)}
             return jsonify(apiresult)
-        apiresult = {'success': True, 'message': "Phone Added Successfully", 'uuid': axlresult['return']}
+        apiresult = {'success': True, 'message': "Phone Added Successfully",
+                     'phone_uuid': axl_add_phone_result['return'],
+                     'user_uuid': axl_update_user_result['return']}
         return jsonify(apiresult)
 
-
-@api.route("/get_phone/<device_name>")
-class cucm_get_phone_api(Resource):
     def get(self, device_name):
         """
-        Finds a phone device from CUCM
+        Retrieves a Phone device configuration from CUCM
+
+        This API method executes an getPhone AXL Request with the supplied device_name
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_getPhone.html
         """
         try:
             axlresult = myAXL.get_phone(device_name)
@@ -118,12 +142,13 @@ class cucm_get_phone_api(Resource):
         apiresult = {'success': True, 'message': "Phone Data Retrieved Successfully", 'phone_data': axlresult['return']['phone']}
         return jsonify(apiresult)
 
-
-@api.route("/delete_phone/<device_name>")
-class cucm_delete_phone_api(Resource):
     def delete(self, device_name):
         """
-        Deletes a phone device from CUCM
+        Deletes a Phone device from CUCM
+
+        This API method executes an removePhone AXL Request with the supplied device_name
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_removePhone.html
         """
         try:
             axlresult = myAXL.delete_phone(device_name)
@@ -133,12 +158,41 @@ class cucm_delete_phone_api(Resource):
         apiresult = {'success': True, 'message': "Phone Successfully Deleted", 'uuid': axlresult['return']}
         return jsonify(apiresult)
 
+    @api.expect(cucm_update_phone_query_args, validate=True)
+    def put(self, device_name):
+        """
+        Updates a Phone Device configuration and Applies it on CUCM
 
-@api.route("/apply_phone/<device_name>")
+        This API method executes an updatePhone and applyPhone AXL Requests with the supplied phone_update_data parameter
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_updatePhone.html
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_applyPhone.html
+        """
+        try:
+            cucm_update_phone_query_parsed_args = cucm_update_phone_query_args.parse_args(request)
+            phone_update_data = {
+                "name": device_name,
+                "description": cucm_update_phone_query_parsed_args["description"],
+                "isActive": cucm_update_phone_query_parsed_args["isActive"],
+                "callingSearchSpaceName": cucm_update_phone_query_parsed_args["callingSearchSpaceName"]
+            }
+            axl_update_phone_result = myAXL.update_phone(phone_data=phone_update_data)
+            myAXL.apply_phone(device_name)
+        except Exception as e:
+            apiresult = {'success': False, 'message': str(e)}
+            return jsonify(apiresult)
+        apiresult = {'success': True, 'message': "Phone Configuration Updated & Applied Successfully",
+                     'uuid': axl_update_phone_result['return']}
+        return jsonify(apiresult)
+
+
+@api.route("/apply_phone/<string:device_name>")
+@api.param('device_name', description='The Name of the Phone Device')
 class cucm_apply_phone_api(Resource):
     def put(self, device_name):
         """
-        Applies a Phone Configuration on CUCM
+        Applies a Phone Device Configuration on CUCM
         """
         try:
             axlresult = myAXL.apply_phone(device_name)
@@ -149,12 +203,17 @@ class cucm_apply_phone_api(Resource):
         return jsonify(apiresult)
 
 
-@api.route("/list_phones")
+@api.route("/phones")
 class cucm_list_phone_api(Resource):
     @api.expect(cucm_list_phones_search_criteria_query_args, cucm_list_phones_returned_tags_query_args, validate=True)
     def get(self):
         """
-        Lists all phone details from CUCM given the search criteria
+        Lists all provisioned phone details from CUCM
+
+        This API method executes listPhone AXL Request with the supplied search criteria
+        <br>
+        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_listPhone.html#Link986
+
         """
         try:
             list_phones_search_criteria_query_parsed_args = cucm_list_phones_search_criteria_query_args.parse_args(request)
@@ -171,35 +230,6 @@ class cucm_list_phone_api(Resource):
         apiresult = {'success': True, 'message': "Phone List Retrieved Successfully",
                      'phone_list_count': len(axlresult['return']['phone']),
                      'phone_list_data': axlresult['return']['phone']}
-        return jsonify(apiresult)
-
-
-@api.route("/edit_phone/<device_name>")
-class cucm_edit_phone_api(Resource):
-    @api.expect(cucm_update_phone_query_args, validate=True)
-    def put(self, device_name):
-        """
-        Updates a Phone Configuration on CUCM
-
-        This API method executes an updatePhone AXL Request with the supplied phone_update_data parameter
-
-        https://pubhub.devnetcloud.com/media/axl-schema-reference/docs/Files/AXLSoap_updatePhone.html#Link105E
-
-        """
-        try:
-            cucm_update_phone_query_parsed_args = cucm_update_phone_query_args.parse_args(request)
-            cucm_update_phone_query_parsed_args['name'] = device_name
-            phone_update_data = {
-                "name": device_name,
-                "description": cucm_update_phone_query_parsed_args["description"],
-                "isActive": cucm_update_phone_query_parsed_args["isActive"],
-                "callingSearchSpaceName": cucm_update_phone_query_parsed_args["callingSearchSpaceName"]
-            }
-            axlresult = myAXL.update_phone(phone_data=phone_update_data)
-        except Exception as e:
-            apiresult = {'success': False, 'message': str(e)}
-            return jsonify(apiresult)
-        apiresult = {'success': True, 'message': "Phone Configuration Updated Successfully", 'uuid': axlresult['return']}
         return jsonify(apiresult)
 
 
