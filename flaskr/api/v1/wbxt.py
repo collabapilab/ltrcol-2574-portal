3f8f7887a79c0599d4c6787821dd6d8935a6c94d
Markus Schneider <marschne@cisco.com>
2020-01-16 23:06:49 -0500

    Webex Teams minor add.

--- flaskr/api/v1/wbxt.py
+++ flaskr/api/v1/wbxt.py
@@ -25,22 +25,28 @@ class wbxt_send_api(Resource):
         try:
             # Get the Rooms list (only group type, not direct)
             rooms = api.rooms.list(type='group')
-
-            # Search through the Rooms to match the room title
-            for room in rooms:
-                if args['room_name'].strip() == room.title.strip():
-                    # Found the room, send a message
-                    api.messages.create(roomId=room.id, text=args['text'])
-                    return {'success': True, 
-                            'messages': 'Successfully sent message', 
-                            'response': ''}
-            # Room was not found
-            return {'success': False, 
-                    'messages': 'Could not find Room "{}"'.format(args['room_name']),
-                    'response': ''}
-
-        # Return any API error that may have been raised
         except ApiError as e:
+             # Return any API error that may have been raised
             return {'success': False,
-                    'messages': 'ApiError encountered',
+                    'messages': 'Error getting Rooms List',
                     'response': '{}'.format(e)}
+
+        # Search through the Rooms to match the room title
+        for room in rooms:
+            if args['room_name'].strip() == room.title.strip():
+                try:
+                    # Found the room, send a message
+                    message = api.messages.create(roomId=room.id, text=args['text'])
+                    return {'success': True, 
+                            'messages': 'Successfully sent message {}'.format(message.id), 
+                            'response': ''}
+                except ApiError as e:
+                    # Return any API error that may have been raised
+                    return {'success': False,
+                            'messages': 'Error sending message',
+                            'response': '{}'.format(e)}
+
+        # Room was not found
+        return {'success': False, 
+                'messages': 'Could not find Room "{}"'.format(args['room_name']),
+                'response': ''}
