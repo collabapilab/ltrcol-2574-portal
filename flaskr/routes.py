b0093cedda0e3dda701a153554e457d1f341c846
Paul Giralt <pgiralt@cisco.com>
2020-01-27 23:46:18 +0100

    remove unneeded code

--- flaskr/routes.py
+++ flaskr/routes.py
@@ -17,10 +17,3 @@ def index():
 def provisioning_route():
     return render_template('provisioning.html',
                            title='LTRCOL-2574 Portal - Provisioning')
-
-
-@core.route('/', defaults={'path': ''})
-@core.route('/<path:path>')
-def catch_all(path):
-    return render_template(path,
-                           title='LTRCOL-2574 Sample Portal')
